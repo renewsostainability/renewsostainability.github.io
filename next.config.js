faf5ddeb6e3408d7fb9fe5f1fd94e99a3270c64e
Tomiwa Kunle Oluwadare <cafeat9ja@gmail.com>
2025-11-21 18:57:09 +0000

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  reactStrictMode: true,
+  assetPrefix: "https://tomiwaoluwadare.github.io/out/",
 };
 
 module.exports = nextConfig;
